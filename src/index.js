e6ac46c3d7ea6d827e76e6d40ab10f20e92165c4
Jan Tore <jtstolsvik@gmail.com>
2017-01-23 00:13:17 +0100

    "combine reducers"

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import {render} from 'react-dom'
 import {App} from './components/App';
 import {counters} from './reducers/counter';
+import {hideFilter} from './reducers/hideFilter';
 import {createStore}  from 'redux';
 import {Provider} from 'react-redux';
 
-let store = createStore(counters);
+let store = createStore((state = {}, action) => {
+    return {
+        counters: counters(state.counters, action),
+        hideFilter: hideFilter(state.hideFilter, action)
+    }
+});
 
 render(
     <Provider store={store}>
