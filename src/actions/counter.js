8e70bda1264cf9261f7aa3ec0c5815495d751bb0
Jan Tore <jtstolsvik@gmail.com>
2017-01-22 23:50:32 +0100

    flere counters og actions

--- src/actions/counter.js
+++ src/actions/counter.js
@@ -1,14 +1,25 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
+export const ADD_COUNTER = "ADD_COUNTER";
 
-export const increment = () => {
+export const increment = (id) => {
     return {
-        type: INCREMENT
+        type: INCREMENT,
+        id
     }
 };
 
-export const decrement = () => {
+export const decrement = (id) => {
     return {
-        type: DECREMENT
+        type: DECREMENT,
+        id
+    }
+};
+
+let counterId = 0;
+export const addCounter = () => {
+    return {
+        type: ADD_COUNTER,
+        id: counterId++
     }
 };
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,13 +1,44 @@
-import {DECREMENT, INCREMENT} from '../actions/counter';
+import {INCREMENT, DECREMENT, ADD_COUNTER} from '../actions/counter'
 
-export const counter = (state = 0, action) => {
+export const counter = (state, action) => {
     if (action.type === INCREMENT) {
-        return state + 1;
+        if (state.id === action.id) {
+            return {
+                ...state,
+                value: state.value + 1
+            }
+        }
     }
     if (action.type === DECREMENT) {
-        return state - 1;
+        if (state.id === action.id) {
+            return {
+                ...state,
+                value: state.value -1
+            }
+        }
+    }
+    if (action.type === ADD_COUNTER) {
+        return {
+            id: action.id,
+            value: 0
+        }
     }
     return state;
+};
+
+export const counters = (state = [], action) => {
+    switch (action.type) {
+        case INCREMENT:
+        case DECREMENT:
+            return state.map(c => counter(c, action));
+        case ADD_COUNTER:
+            return [
+                ...state,
+                counter(undefined, action)
+            ];
+        default:
+            return state;
+    }
 };
 
 export default counter;
