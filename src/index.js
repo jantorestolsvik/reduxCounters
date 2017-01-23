fa9922f3d40d7316abbd8af926ed3dace5380510
Jan Tore <jtstolsvik@gmail.com>
2017-01-23 20:00:55 +0100

    combineReducers

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import {render} from 'react-dom'
 import {App} from './components/App';
 import {counters} from './reducers/counter';
 import {hideFilter} from './reducers/hideFilter';
-import {createStore}  from 'redux';
+import {createStore, combineReducers}  from 'redux';
 import {Provider} from 'react-redux';
-
-let combineReducers = (reducers) => (state = {}, action) => {
-    return Object.keys(reducers).reduce((newState, key) => {
-            newState[key] = reducers[key](state[key], action);
-            return newState;
-        }
-        , {})
-};
 
 let store = createStore(combineReducers({
     counters,
